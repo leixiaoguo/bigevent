13e972ddd15e4125d1a36a62f1a3c84e2c1d3724
马云 <lxg_04@163.com>
2020-09-10 08:49:13 +0800

    登录页面半成品

---
+++ assets/js/login.js
@@ -0,0 +1,15 @@
+$(function() {
+    $('#reg_zh').on('click', function() {
+        $('.reg').show()
+        $('.login').hide()
+    })
+    $('#dl_zh').on('click', function() {
+        $('.reg').hide()
+        $('.login').show()
+    })
+
+
+
+
+
+})
